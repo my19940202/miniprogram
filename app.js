8b5cefd1a3ce5032a9b03a5c68713531c61c6b82
xishengbo <xishengbo@baidu.com>
2023-07-09 21:27:02 +0800

    23-07-09 首页初版swiper + tcell

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ App({
       console.error('请使用 2.2.3 或以上的基础库以使用云能力');
       console.log('not support wx.cloud');
     } else {
-      console.log('support wx.cloud');
       wx.cloud.init({
         // env 参数说明：
         //  env 参数决定接下来小程序发起的云开发调用（wx.cloud.xxx）会默认请求到哪个云环境的资源

dbf6823b39c69e8d160e90900e89b3448fe72ca1
xishengbo <xishengbo@baidu.com>
2023-07-16 20:59:02 +0800

    23-07-16 使用云函数

--- pages/test/index.js
+++ pages/test/index.js
@@ -5,12 +5,23 @@ Page({
         haveCreateCollection: false
     },
     onLoad() {
-        console.log('this is onLoad')
+        // 数据库记录获取
         const db = wx.cloud.database();
         db.collection('housing_forms').get().then(res => {
             // res.data 包含该记录的数据
             console.log(res.data)
-        })
+        });
+        // 云函数调用测试
+        wx.cloud.callFunction({
+            // 云函数名称
+            name: 'helloworld',
+            // 传给云函数的参数
+            data: {a: 1},
+            success: function(res) {
+                console.log(res.result)
+            },
+            fail: console.error
+          })
     },
     jumpTo(event) {
         wx.navigateTo({
